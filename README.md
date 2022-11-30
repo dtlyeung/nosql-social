5f99e9a3fca1a375f33eb5c21242a770be9953eb
Daniel <danieltlyeung@gmail.com>
2022-11-30 18:47:56 -0500

    Changed most of the code. Accidentally pushed from wrong folders prior

---
+++ README.md
@@ -0,0 +1,31 @@
+# NoSQL Social Network API
+
+## Description
+
+This application is an API for a social network web application where uers can share thoughts, add/remove friends and react to the thoughts of others.
+
+## Installation
+
+To run this application, run :
+
+```
+npm install
+```
+
+After that, run:
+
+```
+npm start
+```
+
+And that's basically it.
+
+## Technology
+
+This application was built using:
+
+- Javascript
+- Node.js
+- Express.js
+- Mongoose
+- MongoDB
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require("express");
-const db = require("./config/connection");
-const routes = require("./routes");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// Connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nosql-social', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+// Log mongoose queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(` ************** Connected on localhost:${PORT} ************* `));
